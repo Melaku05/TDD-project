72c64b87aacfbc36874d9b2394aa414de9252130
melaku05 <melakushiferaw05@gmail.com>
2022-06-30 17:59:18 +0300

    Added TDD test for fizz_buzz

--- solver.rb
+++ solver.rb
@@ -10,4 +10,12 @@ class Solver
   def reverse(word)
     word.reverse
   end
+
+  def fizz_buzz(num)
+    if num % 15 == 0 && num % 3 == 0
+      'FizzBuzz'
+    else
+      num % 3 == 0 ? 'Fizz' : num % 5 == 0 ? 'Buzz' : num
+    end
+  end
 end
