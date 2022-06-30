e11261bdf930ac3012f438bdc53cc7a3d75462cc
melaku05 <melakushiferaw05@gmail.com>
2022-06-30 16:08:06 +0300

    TDD test for factorial method

---
+++ spec/factorial_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require './solver'
+
+describe 'Factorial' do
+  solver = Solver.new
+
+  it 'factorial of 0 should 1' do
+    expect(solver.factorial(0)).to eq(1)
+  end
+end
